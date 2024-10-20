0f0808f49c7535370d2894d9334dac51b2d93c59
furiousme <shuptaroks@gmail.com>
2024-10-20 11:11:34 +0300

    lint: setup prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,17 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
     rules: {
-      "no-console": "warn"
-    }
-   },
+      'no-console': 'warn',
+    },
+  },
   {
     ignores: ['node_modules', 'build'],
   }
-);
+)
